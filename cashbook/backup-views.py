3b6c3a4dc9ba9bec4d0bfa781152310fced728bf
shamimhcp1 <shamimhcp1@gmail.com>
2023-05-21 22:21:43 +0600

    Add views.py changes for date filtering

--- cashbook/backup-views.py
+++ cashbook/backup-views.py
@@ -1,76 +1,41 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import TransactionForm
-from django.db.models import Q, Sum
-from datetime import date, timedelta
+from django.db.models import Q
+
 from cashbook.models import Category, Account, Transaction, TrxType
 
 def index(request):
-    
-    trx_types = TrxType.objects.all()
-    categories = Category.objects.all()
+    acc_cash        = Account.objects.get(name='Cash')
+    trx_all         = Transaction.objects.order_by("-create_date")
+    type_cash_in    = TrxType.objects.get(name='cash_in')
+    type_cash_out   = TrxType.objects.get(name='cash_out')
 
-    # Retrieve filter values from request
-    date_filter = request.GET.get('date-filter', '')
-    trx_type_filter = request.GET.get('trx-type', '')
-    category_filter = request.GET.get('category', '')
+    total_cash_in = 0
+    for trx in trx_all:
+        if trx.trx_type_id == type_cash_in:
+            total_cash_in += trx.amount
+            
+    total_cash_out = 0
+    for trx in trx_all:
+        if trx.trx_type_id == type_cash_out:
+            total_cash_out += trx.amount
 
-    # Filter transactions based on filter values
-    trx_all = Transaction.objects.order_by("-create_date")
-    if date_filter:
-        if date_filter == 'today':
-            trx_all = trx_all.filter(create_date__date=date.today())
-        elif date_filter == 'all-time':
-            # This will return trx_all
-            pass
-        elif date_filter == 'yesterday':
-            trx_all = trx_all.filter(create_date__date=date.today() - timedelta(days=1))
-        elif date_filter == 'this_month':
-            trx_all = trx_all.filter(create_date__month=date.today().month)
-        elif date_filter == 'last_month':
-            trx_all = trx_all.filter(create_date__month=date.today().month - 1)
-        elif date_filter == 'single_day':
-            # You'll need to add logic to handle single-day filtering
-            pass
-        elif date_filter == 'date_range':
-            # You'll need to add logic to handle date range filtering
-            pass
-    else:
-        trx_all = trx_all.filter(create_date__month=date.today().month)
-
-    if trx_type_filter:
-        trx_all = trx_all.filter(trx_type_id=trx_type_filter)
-
-    if category_filter:
-        trx_all = trx_all.filter(category_id=category_filter)
-
-    # Calculate totals and net balance
-    type_cash_in = TrxType.objects.get(name='cash_in')
-    type_cash_out = TrxType.objects.get(name='cash_out')
-
-    total_cash_in = trx_all.filter(trx_type_id=type_cash_in).aggregate(Sum('amount'))['amount__sum'] or 0
-    total_cash_out = trx_all.filter(trx_type_id=type_cash_out).aggregate(Sum('amount'))['amount__sum'] or 0
-
-    acc_cash = Account.objects.get(name='Cash')
+    net_balance = 0
     net_balance = acc_cash.initial_balance + total_cash_in - total_cash_out
 
     context = {
-        'trx_all': trx_all,
-        'trx_types': trx_types,
-        'categories': categories,
-        'net_balance': net_balance,
-        'acc_cash': acc_cash,
-        'total_cash_in': total_cash_in,
+        'trx_all'       : trx_all,
+        'type_cash_in'  : type_cash_in,
+        'type_cash_out' : type_cash_out,
+        'total_cash_in' : total_cash_in,
         'total_cash_out': total_cash_out,
-
-        'type_cash_in': type_cash_in,
-        'type_cash_out': type_cash_out,
-        
-        'date_filter': date_filter,
-        'trx_type_filter': trx_type_filter,
-        'category_filter': category_filter,
-    }
-
+        'acc_cash'      : acc_cash,
+        'net_balance'   : net_balance,
+        'categories'    : Category.objects.all(),
+        'trx_types'     : TrxType.objects.all()
+        }
+    
     return render(request, 'cashbook/index.html', context)
 
 def cash_in(request):
